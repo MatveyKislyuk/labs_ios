a8d619a220b2d29fa9f0b4ec8a5b2ac18490ed6e
Matvey Kislyuk <lologan228@yandex.ru>
2024-07-09 00:05:23 +0300

    3-task

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,34 @@
+import 'package:labs_ios/presentation/blocs/category_event.dart';
 import 'package:flutter/material.dart';
-import 'my_app.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:labs_ios/core/utils/service_locator.dart';
+import 'package:labs_ios/presentation/screens/categories_screen.dart';
+import 'package:labs_ios/presentation/blocs/category_bloc.dart';
+import 'package:labs_ios/presentation/blocs/task_bloc.dart';
+import 'package:get_it/get_it.dart';
 
 void main() {
-  runApp(MyApp());
+  init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<CategoryBloc>(
+          create: (context) => GetIt.I<CategoryBloc>()..add(LoadCategories()),
+        ),
+        BlocProvider<TaskBloc>(
+          create: (context) => GetIt.I<TaskBloc>(),
+        ),
+      ],
+      child: MaterialApp(
+        home: const CategoriesScreen(),
+      ),
+    );
+  }
 }
